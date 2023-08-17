b035ed47c3e8e7ca06a88012c3f3f598c6e2959e
nobel7761 <habiburnobel@gmail.com>
2023-08-17 15:14:58 +0600

    list box completed

--- src/components/CustomListbox.tsx
+++ src/components/CustomListbox.tsx
@@ -5,71 +5,88 @@ import { Command, CommandGroup, CommandItem } from "./ui/command";
 import { cn } from "../lib/utils";
 import { IoIosArrowDown } from "react-icons/io";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
-
-const CustomListbox = ({ maxWidth = "w-1/4" }) => {
+const CustomListbox = ({
+  maxWidth = "w-1/4",
+  label,
+  labelClass,
+  buttonClass = "border border-gray-300 py-1 w-full",
+  staticPlaceholder = "Please Select...",
+  data,
+  buttonIconHide = false,
+  dropdownClass = "w-[380px]",
+}: {
+  maxWidth?: string;
+  label?: string;
+  labelClass?: string;
+  buttonClass?: string;
+  staticPlaceholder?: string;
+  data: { value: string; label: string }[];
+  buttonIconHide?: boolean;
+  dropdownClass?: string;
+}) => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
+
+  //! main state
+  const [selected, setSelected] = useState(
+    staticPlaceholder ? staticPlaceholder : data[0].label
+  );
+
+  console.log(selected);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <span
-          className={`${maxWidth} border border-gray-300 rounded-md px-3 py-1 flex justify-between items-center cursor-pointer`}
+    <div className={`${maxWidth && maxWidth}`}>
+      {label && (
+        <p
+          className={`text-small font-medium leading-4 text-grayCustom px-4 my-1 ${labelClass}`}
         >
-          {value
-            ? frameworks.find((framework) => framework.value === value)?.label
-            : "Please Select..."}
-          <IoIosArrowDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </span>
-      </PopoverTrigger>
-      <PopoverContent className={`w-[380px] p-0`}>
-        <Command>
-          <CommandGroup>
-            {frameworks.map(
-              (framework: { value: string; label: string }, index: number) => (
-                <CommandItem
-                  key={index}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === framework.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {framework.label}
-                </CommandItem>
-              )
+          {label}
+        </p>
+      )}
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger
+          asChild
+          className={`relative pl-3 pr-10 text-left text-grayCustom ${buttonClass}`}
+          defaultValue="astro"
+        >
+          <span
+            className={`${maxWidth} border border-gray-300 rounded-md px-3 py-1 flex justify-between items-center cursor-pointer`}
+          >
+            {selected}
+            {buttonIconHide === false && (
+              <IoIosArrowDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
             )}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+          </span>
+        </PopoverTrigger>
+        <PopoverContent className={`p-0 ${dropdownClass}`}>
+          <Command>
+            <CommandGroup className="p-0">
+              {data.map(
+                (item: { value: string; label: string }, index: number) => (
+                  <CommandItem
+                    key={index}
+                    onSelect={(currentValue) => {
+                      setSelected(
+                        currentValue === label ? selected : currentValue
+                      );
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        selected === item.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+
+                    {item.label}
+                  </CommandItem>
+                )
+              )}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 };
 
